742e5c3b07ef720e669da7f778f6cd140ab2799d
Nathan Walker <walkerrunpdx@gmail.com>
2022-07-04 11:24:10 -0700

    chore: ng updates for mlkit-core

--- packages/mlkit-core/angular/index.ts
+++ packages/mlkit-core/angular/index.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { registerElement } from '@nativescript/angular';
 
 import { MLKitViewDirective } from './mlkit-view-directive';
-
+export * from './mlkit-view-directive';
 
 @NgModule({
     declarations: [MLKitViewDirective],
