70879224099f87544aa46ca19c2bedac71233313
Osei Fortune <fortune.osei@yahoo.com>
2023-03-27 23:32:37 -0400

    feat: v2

--- packages/mlkit-core/typings/objc!MLKitTextRecognitionCommon.d.ts
+++ packages/mlkit-core/typings/objc!MLKitTextRecognitionCommon.d.ts
@@ -1,89 +1,83 @@
-
 declare class MLKCommonTextRecognizerOptions extends NSObject {
+  static alloc(): MLKCommonTextRecognizerOptions; // inherited from NSObject
 
-	static alloc(): MLKCommonTextRecognizerOptions; // inherited from NSObject
-
-	static new(): MLKCommonTextRecognizerOptions; // inherited from NSObject
+  static new(): MLKCommonTextRecognizerOptions; // inherited from NSObject
 }
 
 declare class MLKText extends NSObject {
+  static alloc(): MLKText; // inherited from NSObject
 
-	static alloc(): MLKText; // inherited from NSObject
+  static new(): MLKText; // inherited from NSObject
 
-	static new(): MLKText; // inherited from NSObject
+  readonly blocks: NSArray<MLKTextBlock>;
 
-	readonly blocks: NSArray<MLKTextBlock>;
-
-	readonly text: string;
+  readonly text: string;
 }
 
 declare class MLKTextBlock extends NSObject {
+  static alloc(): MLKTextBlock; // inherited from NSObject
 
-	static alloc(): MLKTextBlock; // inherited from NSObject
+  static new(): MLKTextBlock; // inherited from NSObject
 
-	static new(): MLKTextBlock; // inherited from NSObject
+  readonly cornerPoints: NSArray<NSValue>;
 
-	readonly cornerPoints: NSArray<NSValue>;
+  readonly frame: CGRect;
 
-	readonly frame: CGRect;
+  readonly lines: NSArray<MLKTextLine>;
 
-	readonly lines: NSArray<MLKTextLine>;
+  readonly recognizedLanguages: NSArray<MLKTextRecognizedLanguage>;
 
-	readonly recognizedLanguages: NSArray<MLKTextRecognizedLanguage>;
-
-	readonly text: string;
+  readonly text: string;
 }
 
 declare class MLKTextElement extends NSObject {
+  static alloc(): MLKTextElement; // inherited from NSObject
 
-	static alloc(): MLKTextElement; // inherited from NSObject
+  static new(): MLKTextElement; // inherited from NSObject
 
-	static new(): MLKTextElement; // inherited from NSObject
+  readonly cornerPoints: NSArray<NSValue>;
 
-	readonly cornerPoints: NSArray<NSValue>;
+  readonly frame: CGRect;
 
-	readonly frame: CGRect;
+  readonly recognizedLanguages: NSArray<MLKTextRecognizedLanguage>;
 
-	readonly text: string;
+  readonly text: string;
 }
 
 declare class MLKTextLine extends NSObject {
+  static alloc(): MLKTextLine; // inherited from NSObject
 
-	static alloc(): MLKTextLine; // inherited from NSObject
+  static new(): MLKTextLine; // inherited from NSObject
 
-	static new(): MLKTextLine; // inherited from NSObject
+  readonly cornerPoints: NSArray<NSValue>;
 
-	readonly cornerPoints: NSArray<NSValue>;
+  readonly elements: NSArray<MLKTextElement>;
 
-	readonly elements: NSArray<MLKTextElement>;
+  readonly frame: CGRect;
 
-	readonly frame: CGRect;
+  readonly recognizedLanguages: NSArray<MLKTextRecognizedLanguage>;
 
-	readonly recognizedLanguages: NSArray<MLKTextRecognizedLanguage>;
-
-	readonly text: string;
+  readonly text: string;
 }
 
 declare class MLKTextRecognizedLanguage extends NSObject {
+  static alloc(): MLKTextRecognizedLanguage; // inherited from NSObject
 
-	static alloc(): MLKTextRecognizedLanguage; // inherited from NSObject
+  static new(): MLKTextRecognizedLanguage; // inherited from NSObject
 
-	static new(): MLKTextRecognizedLanguage; // inherited from NSObject
-
-	readonly languageCode: string;
+  readonly languageCode: string;
 }
 
 declare class MLKTextRecognizer extends NSObject {
+  static alloc(): MLKTextRecognizer; // inherited from NSObject
 
-	static alloc(): MLKTextRecognizer; // inherited from NSObject
+  static new(): MLKTextRecognizer; // inherited from NSObject
 
-	static new(): MLKTextRecognizer; // inherited from NSObject
+  static textRecognizer(): MLKTextRecognizer;
 
-	static textRecognizer(): MLKTextRecognizer;
+  static textRecognizerWithOptions(options: MLKCommonTextRecognizerOptions): MLKTextRecognizer;
 
-	static textRecognizerWithOptions(options: MLKCommonTextRecognizerOptions): MLKTextRecognizer;
+  processImageCompletion(image: MLKCompatibleImage, completion: (p1: MLKText, p2: NSError) => void): void;
 
-	processImageCompletion(image: MLKCompatibleImage, completion: (p1: MLKText, p2: NSError) => void): void;
-
-	resultsInImageError(image: MLKCompatibleImage): MLKText;
+  resultsInImageError(image: MLKCompatibleImage): MLKText;
 }
